555338736acb4369fb2e666cef589c188385482c
katayama8000 <tattu.0310@gmail.com>
2023-06-08 22:12:44 +0900

    ref

--- src/pages/Sample/sample7.tsx
+++ src/pages/Sample/sample7.tsx
@@ -4,6 +4,7 @@ import { useRef, useState } from 'react';
 const Sample7 = () => {
   const [text, setText] = useState<string>('');
   const inputEl = useRef<HTMLInputElement>(null);
+  const divEl = useRef<HTMLInputElement>(null);
   const handleOnClick = () => {
     console.log(inputEl.current);
     inputEl.current!.focus();
@@ -22,15 +23,30 @@ const Sample7 = () => {
     return countRef.current++;
   };
 
+  const changeInputColor = () => {
+    inputEl.current!.style.color = 'green';
+  };
+
+  const textBig = () => {
+    inputEl.current!.style.fontSize = '20px';
+  };
+
+  const changeDivRef = () => {
+    divEl.current!.innerText = 'aaaaaaaa';
+  };
+
   return (
     <div className='m-auto mt-20 max-w-3xl'>
       <h1>useRef-2</h1>
       <input ref={inputEl} type='text' value={text} onChange={handleOnChange} />
       <button onClick={handleOnClick}>フォーカスを当てる</button>
-      <div>{count}</div>
+      <button onClick={changeInputColor}>chengeColor</button>
+      <button onClick={textBig}>bigger</button>
+      <div ref={divEl}>{count}</div>
       <button onClick={handleAddNum}>Count1アップ</button>
       <div>{countRef.current}</div>
       <button onClick={handleAddRef}>Count2アップ</button>
+      <button onClick={changeDivRef}>divEl</button>
     </div>
   );
 };
